ae7a9d8cbb399efe92f64852702fc71fc566add8
Ryan <81717668+RyanSea@users.noreply.github.com>
2022-04-04 14:54:56 -0700

    update

--- contracts/Raider.sol
+++ contracts/Raider.sol
@@ -25,56 +25,118 @@ contract Raider is ERC721URIStorage, Ownable {
     //Achievement NFT ID Counter
     uint public achievement_id = 1;
 
-    event RaiderMinted(uint256 indexed discord_id, address indexed user , uint256 indexed nft_id);
-    event NewAchievement(uint indexed discord_id, string achievement);
-    event AchievementMinted(uint indexed discord_id, address indexed user, uint256 indexed nft_id, string achievement);
+    event RaiderMinted(
+        uint256 indexed discord_id, 
+        address indexed user , 
+        uint256 indexed nft_id
+    );
+    event NewAchievement(
+        uint indexed discord_id, 
+        string achievement
+    );
+    event AchievementMinted(
+        uint indexed discord_id, 
+        address indexed user, 
+        uint256 indexed nft_id, 
+        string achievement
+    );
 
-    constructor() ERC721("Raider Guild Avatars","RAIDER") {}
+    constructor() ERC721("Raid Guild Profile","RAIDER") {}
 
-    //Initializes Attribute Struct with Discord ID and Primary + Secondary Skills
-    function createProfile(uint discord_id, string calldata primary, string calldata secondary, address _address, string calldata uri) public onlyOwner{
+    //Initializes Attribute Struct with attributes, maps to Discord ID, and mints RAIDER
+    function createProfile (
+        uint discord_id, 
+        string calldata primary, 
+        string calldata secondary, 
+        address _address, 
+        string calldata metadata
+        ) public onlyOwner {
 
         Attributes memory raider;
         raider.primary_skill = primary;
         raider.secondary_skill = secondary;
         raider._address = _address;
+        raider.raider_id = id;
+
+        _mint(
+            _address,
+            id
+        );
+        _setTokenURI(
+            id, 
+            metadata
+        );
+
         Raiders[discord_id] = raider;
-        mintRaider(discord_id, uri);
-        
-    }
 
-    //Mints Raider NFT
-    function mintRaider(uint discord_id, string calldata uri) public onlyOwner{
-
-        Raiders[discord_id].raider_id = id;
-        _mint(Raiders[discord_id]._address, id);
-        _setTokenURI(id, uri);
-        
-
-        emit RaiderMinted(discord_id, Raiders[discord_id]._address, id);
+        emit RaiderMinted(
+            discord_id, 
+            Raiders[discord_id]._address, 
+            id
+        );
         id++;
     }
 
-    function mintAchievement(uint discord_id, string calldata achievement, string calldata uri) public onlyOwner{
+    //Mints achievment NFT + adds achievement to attribute
+    function mintAchievement (
+        uint discord_id, 
+        string calldata achievement, 
+        string calldata metadata
+        ) public onlyOwner {
 
+        Raiders[discord_id].achievements.push(achievement);
         Raiders[discord_id].achievement_ids.push(achievement_id);
-        _mint(Raiders[discord_id]._address, achievement_id);
-        _setTokenURI(achievement_id, uri);
+        
+        _mint(
+            Raiders[discord_id]._address, 
+            achievement_id
+        );
+        _setTokenURI(
+            achievement_id, 
+            metadata
+        );
 
-        emit AchievementMinted(discord_id, Raiders[discord_id]._address, achievement_id, achievement);
+        emit AchievementMinted(
+            discord_id, 
+            Raiders[discord_id]._address, 
+            achievement_id, 
+            achievement
+        );
         achievement_id++;
     }
 
-    function addAchievement(uint discord_id, string calldata achievement) public onlyOwner{
+    //Adds acheivement to attribute (for achievements that aren't worth an NFT)
+    function addAchievement (
+        uint discord_id, 
+        string calldata achievement
+        ) public onlyOwner {
 
         Raiders[discord_id].achievements.push(achievement);
 
-        emit NewAchievement(discord_id, achievement);
+        emit NewAchievement(
+            discord_id, 
+            achievement
+        );
     }
 
-    function changeAvatar(uint nft_id, string calldata new_uri) public onlyOwner{
+    function changeMetadata (
+        uint nft_id, 
+        string calldata new_metadata
+        ) public onlyOwner {
 
-        _setTokenURI(nft_id, new_uri);
+        _setTokenURI(
+            nft_id, 
+            new_metadata
+        );
+
+    }
+
+    //Unable to view array within struct for strut view call, so this requires its own function
+    function viewAchievements (
+        uint discord_id
+        ) public view  returns (string[] memory) {
+
+        return Raiders[discord_id].achievements;
 
     }
 
